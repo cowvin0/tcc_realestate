678ded36fe6ac5a5cd547643caed57342d687c63
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-07-17 15:22:44 -0300

    impedindo rodirecionamento automático

--- scrapy_zap/spiders/zap_imoveis.py
+++ scrapy_zap/spiders/zap_imoveis.py
@@ -2,29 +2,28 @@ import scrapy
 from pathlib import Path
 from functools import reduce 
 from  urllib.parse import urljoin
+from scrapy.http import Request
 
 class ZapSpider(scrapy.Spider):
 
     name = 'zap'
     allowed_domains = ['www.zapimoveis.com.br']
-    #start_urls = ['https://www.zapimoveis.com.br/venda/imoveis/rj+rio-de-janeiro']
+    start_urls = ['https://www.zapimoveis.com.br/venda/imoveis/rj+rio-de-janeiro/']
 
     def __init__(self, cidade=None, *args, **kwargs):
 
         super(ZapSpider, self).__init__(*args, **kwargs)
 
-        self.start_urls =[
-                urljoin('https://www.zapimoveis.com.br/venda/imoveis',
-                        cidade if cidade == None else 'rj+rio-de-janeiro/'),
-                ]
+    def start_requests(self):
+        yield Request(
+            url=self.start_urls[0], 
+            meta = {'dont_redirect': True,'handle_httpstatus_list': [302, 308]},
+            callback=self.parse
+            )
 
     def parse(self, response):
-        #tipo_imovel = input('Diga o tipo do imovel: ')
-        #pagina_imovel = 'https://www.zapimoveis.com.br/venda/' + tipo_imovel + '/rj+rio-de-janeiro'
         selecionar_divs = response.css('div')
         coletando_hrefs = [href.css('a.result-card ::attr(href)').getall() for href in selecionar_divs]
-
-
 
         yield {'href': reduce(lambda x, y: x + y, coletando_hrefs)}
 
