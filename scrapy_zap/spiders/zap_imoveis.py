45a37c2a666794987bfcd90091dbe229f0858a65
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-07-18 00:56:09 -0300

    coletando as informações da página de cada imóvel

--- scrapy_zap/spiders/zap_imoveis.py
+++ scrapy_zap/spiders/zap_imoveis.py
@@ -1,7 +1,6 @@
 import scrapy
-from pathlib import Path
 from functools import reduce 
-from  urllib.parse import urljoin
+from urllib.parse import urljoin
 from scrapy.http import Request
 
 class ZapSpider(scrapy.Spider):
@@ -15,6 +14,7 @@ class ZapSpider(scrapy.Spider):
         super(ZapSpider, self).__init__(*args, **kwargs)
 
     def start_requests(self):
+
         yield Request(
             url=self.start_urls[0], 
             meta = {'dont_redirect': True,'handle_httpstatus_list': [302, 308]},
@@ -22,10 +22,27 @@ class ZapSpider(scrapy.Spider):
             )
 
     def parse(self, response):
+
         selecionar_divs = response.css('div')
         coletando_hrefs = [href.css('a.result-card ::attr(href)').getall() for href in selecionar_divs]
 
-        yield {'href': reduce(lambda x, y: x + y, coletando_hrefs)}
+        for url in reduce(lambda x, y: x + y, coletando_hrefs):
+            yield response.follow(url, callback=self.parse_imovel_info)
+            
+            
+        #yield {'href': reduce(lambda x, y: x + y, coletando_hrefs)}
 
     def parse_imovel_info(self, response):
-        pass
+
+        imovel_info = response.css('ul.amenities__list ::text').getall()
+        tipo_imovel = response.css('a.breadcrumb__link--router ::text').get()
+        endereco_imovel = response.css('span.link ::text').get()
+        preco_imovel = response.xpath('//li[@class="price__item--main text-regular text-regular__bolder"]/strong/text()').get()
+        condo_iptu = response.css('span.price__value ::text').getall()
+        area = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorSize"]::text').get()
+        num_quarto = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfRooms"]::text').get()
+        num_banheiro = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfBathroomsTotal"]::text').get()
+        andar = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorLevel"]::text').get()
+        
+
+
