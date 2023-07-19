65e109cd4935d763e5cf5fd25da514a11d8c8ab2
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-07-18 21:40:20 -0300

    corrigindo erros no código

--- scrapy_zap/spiders/zap_imoveis.py
+++ scrapy_zap/spiders/zap_imoveis.py
@@ -38,10 +38,40 @@ class ZapSpider(scrapy.Spider):
         tipo_imovel = response.css('a.breadcrumb__link--router ::text').get()
         endereco_imovel = response.css('span.link ::text').get()
         preco_imovel = response.xpath('//li[@class="price__item--main text-regular text-regular__bolder"]/strong/text()').get()
-        condo_iptu = response.css('span.price__value ::text').getall()
+        condominio = response.xpath('//li[@class="price__item condominium color-dark text-regular"]/span/text()').get()
+        iptu = response.xpath('//li[@class="price__item iptu color-dark text-regular"]/span/text()').get()
         area = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorSize"]::text').get()
         num_quarto = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfRooms"]::text').get()
         num_banheiro = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfBathroomsTotal"]::text').get()
         andar = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorLevel"]::text').get()
-        pass
+        
+        yield {
+                "valor": preco_imovel,
+                "tipo": tipo_imovel,
+                "endereco": endereco_imovel,
+                "condominio": condominio,
+                "iptu": iptu,
+                "area": area,
+                "quarto": num_quarto,
+                "banheiro": num_banheiro,
+                "andar": andar,
+                "academia": list(filter(lambda x: "Academia" in x, imovel_info)),
+                "piscina": list(filter(lambda x: "Piscina" in x, imovel_info)),
+                "spa": list(filter(lambda x: "Spa" in x, imovel_info)),
+                "sauna": list(filter(lambda x: "Sauna" in x, imovel_info)),
+                "varanda_gourmet": list(filter(lambda x: "Varanda Gourmet" in x, imovel_info)),
+                "espaco_gourmet": list(filter(lambda x: "Espaço Gourmet" in x, imovel_info)),
+                "playground": list(filter(lambda x: "Playground" in x, imovel_info)),
+                "portaria_24_horas": list(filter(lambda x: "Portaria 24h" in x, imovel_info)),
+                "area_servico": list(filter(lambda x: "Área de serviço" in x, imovel_info)),
+                "url": response.url
+                }
+
+    #@staticmethod
+    #def info_isin(carac, info):
+
+    #    if info in carac:
+    #        return info
+    #    else:
+    #        None
 
