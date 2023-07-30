54fdef2832cf8ddc95ebe661528e5603f8dfcf7c
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-07-30 20:52:10 -0300

    adicionando código javascript para descer a página automaticamente

--- scrapy_zap/spiders/zap_imoveis.py
+++ scrapy_zap/spiders/zap_imoveis.py
@@ -1,20 +1,22 @@
 import scrapy
+import time
 import random
 import re
 from scrapy_zap.items import ZapItem
+from scrapy.selector import Selector
 from scrapy_playwright.page import PageMethod
 from urllib.parse import urljoin
 from scrapy.http import Request
 
 class ZapSpider(scrapy.Spider):
 
-    async def errback(self, failure): 
-        page =failure.request.meta['playwright_page']
-        await page.closed()
-
     name = 'zap'
     allowed_domains = ['www.zapimoveis.com.br']
-    start_urls = ['https://www.zapimoveis.com.br/venda/imoveis/ma+sao-jose-de-ribamar/?transacao=venda&onde=,Maranh%C3%A3o,S%C3%A3o%20Jos%C3%A9%20de%20Ribamar,,,,,city,BR%3EMaranhao%3ENULL%3ESao%20Jose%20de%20Ribamar,-2.552398,-44.069254,&pagina=1']
+    start_urls = ['https://www.zapimoveis.com.br/venda/imoveis/ma+sao-jose-de-ribamar/?transacao=venda&onde=,Maranh%C3%A3o,S%C3%A3o%20Jos%C3%A9%20de%20Ribamar,,,,,city,BR%3EMaranhao%3ENULL%3ESao%20Jose%20de%20Ribamar,-2.552398,-44.069254,&pagina=' + str(page) for page in range(1, 32)]
+
+    async def errback(self, failure): 
+        page = failure.request.meta['playwright_page']
+        await page.closed()
 
     def __init__(self, cidade=None, *args, **kwargs):
         super(ZapSpider, self).__init__(*args, **kwargs)
@@ -30,73 +32,127 @@ class ZapSpider(scrapy.Spider):
                         playwright = True,
                         playwright_include_page = True,
                         playwright_page_methods = {
-                            'evaluate': PageMethod('evaluate', 'Array.from(document.querySelectorAll("a.result-card")).map(a => a.href)')
+                            'scroll_down': PageMethod('evaluate', r'''
+                                                      (async () => {
+                                                          const scrollStep = 20;
+                                                          const delay = 16;
+                                                          let currentPosition = 0;
+
+                                                          function animateScroll() {
+                                                              const pageHeight = Math.max(
+                                                                  document.body.scrollHeight, document.documentElement.scrollHeight,
+                                                                  document.body.offsetHeight, document.documentElement.offsetHeight,
+                                                                  document.body.clientHeight, document.documentElement.clientHeight
+                                                                  );
+
+                                                              if (currentPosition < pageHeight) {
+                                                                  currentPosition += scrollStep;
+                                                                  if (currentPosition > pageHeight) {
+                                                                      currentPosition = pageHeight;
+                                                                  }
+                                                                  window.scrollTo(0, currentPosition);
+                                                                  requestAnimationFrame(animateScroll);
+                                                                  }
+                                                              }
+                                                          animateScroll();
+                                                          })();
+                                                      '''),
+                            'get_href': PageMethod('evaluate', 'Array.from(document.querySelectorAll("a.result-card")).map(a => a.href)'),
                             },
                         errback = self.errback
                         ),
                     callback=self.parse
                     )
             
-    def parse(self, response):
-        hrefs = response.meta['playwright_page_methods']['evaluate'].result
-        yield {'href': hrefs}
+    async def parse(self, response):
 
-        #for url in hrefs:
-        #    yield response.follow(url, callback=self.parse_imovel_info,
-        #                          dont_filter = True
-        #                          )
+        page = response.meta['playwright_page']
+        playwright_page_methods = response.meta['playwright_page_methods']
 
-    #def parse_imovel_info(self, response):
+        await page.evaluate(
+                '''
+                var intervalID = setInterval(function () {
+                    var ScrollingElement = (document.scrollingElement || document.body);
+                    scrollingElement.scrollTop += 20;
+                    }, 100);
+                '''
+                )
 
-    #    zap_item = ZapItem()
+        prev_height = None
+        while True:
+            curr_height = await page.evaluate('(window.innerHeight + window.scrollY)')
+            if not prev_height:
+                prev_height = curr_height
+                time.sleep(1)
+            elif prev_height == curr_height:
+                await page.evaluate('clearInterval(intervalID)')
+                break
+            else:
+                prev_height = curr_height
+                time.sleep(1)
 
-    #    imovel_info = response.css('ul.amenities__list ::text').getall()
-    #    tipo_imovel = response.css('a.breadcrumb__link--router ::text').get()
-    #    endereco_imovel = response.css('span.link ::text').get()
-    #    preco_imovel = response.xpath('//li[@class="price__item--main text-regular text-regular__bolder"]/strong/text()').get()
-    #    condominio = response.xpath('//li[@class="price__item condominium color-dark text-regular"]/span/text()').get()
-    #    iptu = response.xpath('//li[@class="price__item iptu color-dark text-regular"]/span/text()').get()
-    #    area = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorSize"]::text').get()
-    #    num_quarto = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfRooms"]::text').get()
-    #    num_banheiro = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfBathroomsTotal"]::text').get()
-    #    num_vaga = response.xpath('//ul[@class="feature__container info__base-amenities"]/li[@class="feature__item text-regular js-parking-spaces"]/span/text()').get()
-    #    andar = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorLevel"]::text').get()
-    #    url = response.url
-    #    id = re.search(r'id-(\d+)/', url).group(1)
+        hrefs = await page.evaluate('Array.from(document.querySelectorAll("a.result-card")).map(a => a.href)')
 
-    #    filtering = lambda info: [check if info == check.replace('\n', '').lower().strip() else None for check in imovel_info]
+        await page.close()
 
-    #    lista = {
-    #            'academia': list(filter(lambda x: "academia" in x.lower(), imovel_info)),
-    #            'piscina': list(filter(lambda x: x != None, filtering('piscina'))),
-    #            'spa': list(filter(lambda x: x != None, filtering('spa'))),
-    #            'sauna': list(filter(lambda x: "sauna" in x.lower(), imovel_info)),
-    #            'varanda_gourmet': list(filter(lambda x: "varanda gourmet" in x.lower(), imovel_info)),
-    #            'espaco_gourmet': list(filter(lambda x: "espaço gourmet" in x.lower(), imovel_info)),
-    #            'quadra_de_esporte': list(filter(lambda x: 'quadra poliesportiva' in x.lower(), imovel_info)),
-    #            'playground': list(filter(lambda x: "playground" in x.lower(), imovel_info)),
-    #            'portaria_24_horas': list(filter(lambda x: "portaria 24h" in x.lower(), imovel_info)),
-    #            'area_servico': list(filter(lambda x: "área de serviço" in x.lower(), imovel_info)),
-    #            'elevador': list(filter(lambda x: "elevador" in x.lower(), imovel_info))
-    #            }
+        yield {'href': len(hrefs)}
 
-    #    for info, conteudo in lista.items():
-    #        if len(conteudo) == 0:
-    #            zap_item[info] = None
-    #        else:
-    #            zap_item[info] = conteudo[0]
+        for url in hrefs:
+            yield response.follow(url, callback=self.parse_imovel_info,
+                                  dont_filter = True
+                                 )
 
-    #    zap_item['valor'] = preco_imovel,
-    #    zap_item['tipo'] = tipo_imovel,
-    #    zap_item['endereco'] = endereco_imovel.replace('\n', '').strip(),
-    #    zap_item['condominio'] = condominio,
-    #    zap_item['iptu'] = iptu,
-    #    zap_item['area'] = area,
-    #    zap_item['quarto'] = num_quarto,
-    #    zap_item['vaga'] = num_vaga,
-    #    zap_item['banheiro'] = num_banheiro,
-    #    zap_item['andar'] = andar,
-    #    zap_item['url'] = response.url,
-    #    zap_item['id'] = int(id)
-    #    
-    #    yield zap_item
+    def parse_imovel_info(self, response):
+
+        zap_item = ZapItem()
+
+        imovel_info = response.css('ul.amenities__list ::text').getall()
+        tipo_imovel = response.css('a.breadcrumb__link--router ::text').get()
+        endereco_imovel = response.css('span.link ::text').get()
+        preco_imovel = response.xpath('//li[@class="price__item--main text-regular text-regular__bolder"]/strong/text()').get()
+        condominio = response.xpath('//li[@class="price__item condominium color-dark text-regular"]/span/text()').get()
+        iptu = response.xpath('//li[@class="price__item iptu color-dark text-regular"]/span/text()').get()
+        area = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorSize"]::text').get()
+        num_quarto = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfRooms"]::text').get()
+        num_banheiro = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="numberOfBathroomsTotal"]::text').get()
+        num_vaga = response.xpath('//ul[@class="feature__container info__base-amenities"]/li[@class="feature__item text-regular js-parking-spaces"]/span/text()').get()
+        andar = response.xpath('//ul[@class="feature__container info__base-amenities"]/li').css('span[itemprop="floorLevel"]::text').get()
+        url = response.url
+        id = re.search(r'id-(\d+)/', url).group(1)
+
+        filtering = lambda info: [check if info == check.replace('\n', '').lower().strip() else None for check in imovel_info]
+
+        lista = {
+                'academia': list(filter(lambda x: "academia" in x.lower(), imovel_info)),
+                'piscina': list(filter(lambda x: x != None, filtering('piscina'))),
+                'spa': list(filter(lambda x: x != None, filtering('spa'))),
+                'sauna': list(filter(lambda x: "sauna" in x.lower(), imovel_info)),
+                'varanda_gourmet': list(filter(lambda x: "varanda gourmet" in x.lower(), imovel_info)),
+                'espaco_gourmet': list(filter(lambda x: "espaço gourmet" in x.lower(), imovel_info)),
+                'quadra_de_esporte': list(filter(lambda x: 'quadra poliesportiva' in x.lower(), imovel_info)),
+                'playground': list(filter(lambda x: "playground" in x.lower(), imovel_info)),
+                'portaria_24_horas': list(filter(lambda x: "portaria 24h" in x.lower(), imovel_info)),
+                'area_servico': list(filter(lambda x: "área de serviço" in x.lower(), imovel_info)),
+                'elevador': list(filter(lambda x: "elevador" in x.lower(), imovel_info))
+                }
+
+        for info, conteudo in lista.items():
+            if len(conteudo) == 0:
+                zap_item[info] = None
+            else:
+                zap_item[info] = conteudo[0]
+
+        zap_item['valor'] = preco_imovel,
+        zap_item['tipo'] = tipo_imovel,
+        zap_item['endereco'] = endereco_imovel.replace('\n', '').strip(),
+        zap_item['condominio'] = condominio,
+        zap_item['iptu'] = iptu,
+        zap_item['area'] = area,
+        zap_item['quarto'] = num_quarto,
+        zap_item['vaga'] = num_vaga,
+        zap_item['banheiro'] = num_banheiro,
+        zap_item['andar'] = andar,
+        zap_item['url'] = response.url,
+        zap_item['id'] = int(id)
+        
+        yield zap_item
