97b84d4fe33c33f6343c60e902672f158f48f66a
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-09-05 14:24:28 -0300

    atualizando script de limpeza e subindo dados com geocodificação pronta

--- scripts/limpeza.py
+++ scripts/limpeza.py
@@ -6,13 +6,15 @@ dados = pd.read_csv(place)
 
 def limpar(df):
 
-    remove_cols = df[df.academia != "academia"].\
+    remove_cols = df[df.academia != "academia" and df.tipo != "imoveis"].\
             drop_duplicates(["id"]).\
             replace({"lancamentos_de_terrenos_lotes_e_condominios": "terrenos_lotes_e_condominios",
                      "lancamentos_de_casas_de_condominio": "casas_de_condominio",
                      "lancamentos_de_apartamentos": "apartamentos",
                      "lancamentos_de_casas_comerciais": "casas_comerciais",
                      "lancamentos_de_casas": "casas"}).\
+            query("sauna == 1 or spa == 1 or quadra_de_esporte == 1 or varanda_gourmet == 1 or academia == 1").\
+            query("valor > 230000").\
             reset_index(drop=True)
             
 
