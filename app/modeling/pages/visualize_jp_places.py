46f2eaa1835515ab226b6b14f9e1a096ba13f331
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2025-01-27 18:48:56 -0300

    refac code visu_jp

--- app/modeling/pages/visualize_jp_places.py
+++ app/modeling/pages/visualize_jp_places.py
@@ -2,245 +2,156 @@ import geopandas as gpd
 import folium
 import dash
 import dash_bootstrap_components as dbc
-from dash import html, callback, Input, Output
+import os
 
-dash.register_page(
-    __name__,
-    name='Localidades de João Pessoa',
-    path='/locals'
-)
+from folium.plugins import MarkerCluster
+from dash import html, callback, Input, Output, ctx, dcc, ALL
 
-sidebar = html.Div(
-    [
+dash.register_page(__name__, name="Locais importantes", path="/locals")
+
+
+def get_available_datasets():
+    city_folder = "app/modeling/assets/geo_joao_pessoa"
+    if not os.path.exists(city_folder):
+        return []
+    return [
+        os.path.splitext(file)[0]
+        for file in os.listdir(city_folder)
+        if file.endswith(".geojson")
+    ]
+
+
+def generate_sidebar():
+    datasets = get_available_datasets()
+    buttons = []
+    for dataset in datasets:
+        label = dataset.replace("_", " ").capitalize()
+        button_id = {"type": "dynamic-button", "dataset": dataset}
+        buttons.append(
+            dbc.Col(
+                dbc.Button(
+                    html.Span(
+                        dbc.Stack(
+                            [
+                                html.I(className="fas fa-map-marker-alt ml-2"),
+                                f" {label}",
+                            ]
+                        )
+                    ),
+                    id=button_id,
+                    color="light",
+                    className="m-1",
+                    style={"width": "100%"},
+                ),
+                xs=12,
+                sm=6,
+            )
+        )
+
+    sidebar_content = [
         html.H2(
-            "Localidades de JP",
-            className="display-5",
-            style={"font-weight": "bold"}),
+            "Pontos estratégicos", className="display-5", style={"font-weight": "bold"}
+        ),
         html.P(
-            "Veja dados geográficos da cidade de João Pessoa, "
-            "como bairros, praças, parques, escolas, ciclovias etc.",
+            "Veja dados geográficos de João Pessoa, como bairros, praças, "
+            "parques, escolas, etc.",
             className="lead",
             style={"textAlign": "justify"},
         ),
-        html.Div(
-            [
-                dbc.Row([
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-map ml-2"),
-                            " Bairros"
-                        ])),
-                        id="btn-bairros",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
+        dbc.Row(buttons),
+    ]
 
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-leaf ml-2"),
-                            " Praças"
-                        ])),
-                        id="btn-pracas",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-tree ml-2"),
-                            " Parques"
-                        ])),
-                        id="btn-parques",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-water ml-2"),
-                            " Rios"
-                        ])),
-                        id="btn-rios",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-bicycle ml-2"),
-                            " Ciclovário"
-                        ])),
-                        id="btn-ciclo",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-bus ml-2"),
-                            " Faixas exclusivas"
-                        ])),
-                        id="btn-faixas_exclusivas",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-road ml-2"),
-                            " Corredores"
-                        ])),
-                        id="btn-corredores",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-school ml-2"),
-                            " Escolas públicas"
-                        ])),
-                        id="btn-escolas_publicas",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-solid fa-home ml-2"),
-                            " Comunidades"
-                        ])),
-                        id="btn-comunidades",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-
-                    dbc.Col(dbc.Button(
-                        html.Span(dbc.Stack([
-                            html.I(className="fas fa-sun ml-2"),
-                            " Área rural"
-                        ])),
-                        id="btn-area_rural",
-                        color="light",
-                        className="m-1",
-                        style={"width": "100%"},
-                    ), width=6),
-                ]),
-            ]
-        ),
-    ],
-    style={
-        "padding": "20px",
-        "background-color": "#f8f9fa",
-        "height": "100vh",
-        "width": "20%",
-        "position": "fixed",
-        "left": 0,
-        "top": "56px",
-        "overflow-y": "auto",
-    },
-)
+    return html.Div(
+        sidebar_content,
+        style={
+            "padding": "20px",
+            "background-color": "#f8f9fa",
+            "height": "100vh",
+            "width": "100%",
+            "max-width": "300px",
+            "position": "fixed",
+            "left": 0,
+            "top": "56px",
+            "overflow-y": "auto",
+        },
+    )
 
 
 def generate_map(map_type):
     m = folium.Map(
-      location=[-7.15, -34.85],
-      zoom_control=True,
-      attribution_control=False,
-      zoom_start=12)
+        location=[-7.15, -34.85],
+        zoom_control=True,
+        attribution_control=False,
+        zoom_start=12,
+    )
 
-    geo_data = gpd.read_file(f"app/modeling/assets/{map_type}" + ".geojson")
-    folium.GeoJson(geo_data, name="hello world").add_to(m)
+    city_folder = f"app/modeling/assets/geo_joao_pessoa/{map_type}.geojson"
+    geo_data = gpd.read_file(city_folder)
+
+    if map_type == "escolas_públicas":
+        marker_cluster = MarkerCluster().add_to(m)
+        for _, row in geo_data.iterrows():
+            folium.Marker(
+                location=[row.geometry.y, row.geometry.x],
+            ).add_to(marker_cluster)
+    else:
+        folium.GeoJson(geo_data, name="geojson").add_to(m)
 
     map_file = m.get_root().render()
     return map_file
 
 
-# FONTE DIN
-
 map_component = html.Div(
-    html.Iframe(
-        id="map-iframe",
-        srcDoc=generate_map('bairros'),
-        style={
-            "height": "92.5vh",
-            "width": "80vw",
-            "border": "none",
+    dcc.Loading(
+        id="loading-map",
+        type="circle",
+        children=html.Iframe(
+            id="map-iframe",
+            srcDoc=generate_map("bairros"),
+            style={
+                "height": "92.5vh",
+                "width": "100%",
+                "border": "none",
             },
+        ),
     ),
     style={
         "position": "fixed",
         "top": "56px",
-        "left": "20%"
-        },
+        "left": "300px",
+        "right": 0,
+    },
 )
 
-layout = [sidebar, map_component]
+
+layout = html.Div(
+    [
+        html.Div(id="sidebar-container", children=generate_sidebar()),
+        map_component,
+        dcc.Store(id="map-config", data={"dataset": "bairros"}),
+    ]
+)
+
+
+@callback(
+    Output("map-config", "data"),
+    Input({"type": "dynamic-button", "dataset": ALL}, "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_map_config(button_clicks):
+    ctx_id = ctx.triggered_id
+
+    if isinstance(ctx_id, dict) and ctx_id.get("type") == "dynamic-button":
+        dataset = ctx_id["dataset"]
+        return {"dataset": dataset}
+
+    return dash.no_update
 
 
 @callback(
     Output("map-iframe", "srcDoc"),
-    [Input("btn-bairros", "n_clicks"),
-     Input("btn-pracas", "n_clicks"),
-     Input("btn-parques", "n_clicks"),
-     Input("btn-area_rural", "n_clicks"),
-     Input("btn-comunidades", "n_clicks"),
-     Input("btn-ciclo", "n_clicks"),
-     Input("btn-corredores", "n_clicks"),
-     Input("btn-escolas_publicas", "n_clicks"),
-     Input("btn-rios", "n_clicks"),
-     Input("btn-faixas_exclusivas", "n_clicks")]
+    Input("map-config", "data"),
 )
-def update_map(
-    n_bairros, n_pracas, n_parques,
-    n_area_rural, n_comunidades,
-    n_ciclo, n_corredores,
-    n_escolas_publicas, n_rios,
-    n_faixas_exclusivas):
-    ctx = dash.callback_context
-    if not ctx.triggered:
-        button_id = "bairros"
-    else:
-        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
-
-    if button_id == "btn-bairros":
-        map_file = generate_map("bairros")
-    elif button_id == "btn-pracas":
-        map_file = generate_map("pracas")
-    elif button_id == "btn-parques":
-        map_file = generate_map("parques")
-    elif button_id == "btn-area_rural":
-        map_file = generate_map("area_rural")
-    elif button_id == "btn-comunidades":
-        map_file = generate_map("comunidades")
-    elif button_id == "btn-ciclo":
-        map_file = generate_map("ciclo")
-    elif button_id == "btn-corredores":
-        map_file = generate_map("corredores")
-    elif button_id == "btn-escolas_publicas":
-        map_file = generate_map("escolas_publicas")
-    elif button_id == "btn-faixas_exclusivas":
-        map_file = generate_map("faixas_exclusivas")
-    elif button_id == "btn-rios":
-        map_file = generate_map("rios")
-    else:
-        map_file = generate_map("bairros")
-
-    return map_file
+def update_map(map_config):
+    dataset = map_config["dataset"]
+    return generate_map(dataset)
