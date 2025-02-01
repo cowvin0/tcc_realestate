9ea8bab57a9a2e8b9cd28e8c870a206c4a7d2a43
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2025-02-01 13:24:00 -0300

    .

--- app/modeling/pages/about.py
+++ app/modeling/pages/about.py
@@ -1,15 +1,8 @@
 import dash
-import dash_bootstrap_components as dbc
-import plotly.express as px
 
-from dash import dcc, html, Input, Output, State, callback
+from dash import html
 
-dash.register_page(
-    __name__,
-    name='Sobre',
-    path='/'
-    )
+dash.register_page(__name__, name="Sobre", path="/")
 
 
-layout = html.Div(
-)
+layout = html.Div()
