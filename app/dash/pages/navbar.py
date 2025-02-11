f323c41ae64b3c0711e27b9987aa358d6bd1ca0d
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2025-02-11 20:07:30 -0300

    add latitude and longitude in offcanvas

--- app/dash/pages/navbar.py
+++ app/dash/pages/navbar.py
@@ -11,39 +11,31 @@ def navbar():
                 html.A(
                     dbc.Row(
                         [
-                            dbc.Col(
-                                html.Img(
-                                    src='./assets/logode.png',
-                                    height="30px"
-                                )
-                            ),
+                            dbc.Col(html.Img(src="./assets/logode.png", height="30px")),
                         ],
                         align="left",
                         className="g-0",
                     ),
-                    href='https://www.ufpb.br/de',
-                    style={'textDecoration': 'none'},
+                    href="https://www.ufpb.br/de",
+                    style={"textDecoration": "none"},
                 ),
-                dbc.NavbarToggler(id='navbar-toggler', n_clicks=0),
+                dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
                 dbc.Collapse(
                     dbc.Nav(
                         [
-                            dbc.NavLink(
-                                page['name'],
-                                href=page['path'],
-                                active='exact'
-                            ) for page in dash.page_registry.values()
+                            dbc.NavLink(page["name"], href=page["path"], active="exact")
+                            for page in dash.page_registry.values()
                         ],
                         className="ms-auto",
                         navbar=True,
                     ),
-                    id='navbar-collapse',
+                    id="navbar-collapse",
                     is_open=False,
                     navbar=True,
                 ),
             ]
         ),
-        color='secondary',
+        color="secondary",
         dark=True,
     )
 
